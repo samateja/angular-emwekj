776096842cb980128fa63118026c48cc0480a587
samateja <samavteja@gmail.com>
2020-09-17 16:05:43 +0200

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { Message, User, SendMessageEvent } from '@progress/kendo-angular-conversational-ui';
+import { Component } from "@angular/core";
+import {
+  Message,
+  User,
+  SendMessageEvent
+} from "@progress/kendo-angular-conversational-ui";
+import { HttpClient } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
+import { catchError, retry } from "rxjs/operators";
 
 const bot: User = {
   id: 0
@@ -10,32 +17,44 @@ const user: User = {
 };
 
 const hello: Message = {
-  text: 'Hello!',
+  text: "Hello!",
   author: bot
 };
 
 @Component({
-    selector: 'my-app',
-    template: `
-      <kendo-chat
-        [messages]="feed"
-        [user]="user"
-        (sendMessage)="sendMessage($event)"
-      >
-      </kendo-chat>
-    `
+  selector: "my-app",
+  template: `
+    <kendo-chat
+      [messages]="feed"
+      [user]="user"
+      (sendMessage)="getMessage($event)"
+    >
+    </kendo-chat>
+  `
 })
 export class AppComponent {
-  public feed: Message[] = [ hello ];
+  public feed: Message[] = [hello];
   public readonly user: User = user;
   public readonly bot: User = bot;
+  public url: string = "https://jsonplaceholder.typicode.com/posts";
+
+  constructor(private http: HttpClient) {}
+
+  public getMessage(e: SendMessageEvent) {
+    this.http
+      .post<any>(this.url, { message: e.message.text })
+      .subscribe(data => {
+        e.message.text = data.message;
+        this.sendMessage(e);
+      });
+  }
 
   public sendMessage(e: SendMessageEvent): void {
     const echo: Message = {
       author: bot,
-      text: `You said: "${ e.message.text }"`
+      text: `You said: "${e.message.text}"`
     };
 
     this.feed = [...this.feed, e.message, echo];
   }
- }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChatModule } from '@progress/kendo-angular-conversational-ui';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, ChatModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, ChatModule, HttpClientModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
